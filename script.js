e6ace6ad161bceb91db02b89c1bc96fcde1f3a98
EEdouard28 <111817163+EEdouard28@users.noreply.github.com>
2022-10-02 18:25:56 -0400

    html and css styling  update

---
+++ script.js
@@ -0,0 +1,24 @@
+// // Group of questions being stored
+// var questions = [
+//     {
+//         prompt: "Commonly used data types DO Not Include?\n(a) strings\n\
+//         (b) booleans\n\(c) alerts\n(d) numbers",
+//         answer: "c",
+//     },
+// ]
+// // var score = 0;
+// var score = 0;
+
+// // for loop create to access prompt questions and response
+// for (var i=0; i < questions.length; i++) {
+//     var response = window.prompt (questions[i].prompt)
+//     if(response == questions[i].answer) {
+//         score++;
+//         alert("Correct!");
+//     } else {
+//         alert("Wrong!");
+//     }
+//     }
+
+// // alert providing score total 
+// alert("you got " + score + "/" + questions.length );
